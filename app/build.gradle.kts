12bf12d33415b9e7f4284e8f8155972758cb7d22
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-09-17 00:16:16 +0530

    feat: Firebase Integration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -69,4 +70,14 @@ dependencies {
 
     val nav_version = "2.7.1"
     implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
+    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+
+
 }
---
+++ app/src/main/java/com/example/threads/screens/Login.kt
@@ -0,0 +1,15 @@
+package com.example.threads.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LoginUI() {
+    
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginUIPreview() {
+    LoginUI()
+}
